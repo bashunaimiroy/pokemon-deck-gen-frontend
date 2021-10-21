ec0571802b45fa50cb47a0f8b148f33fc2b66f0a
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2021-10-21 14:05:02 -0400

    add loading state to DeckList

--- pokemon-deck-generator/src/components/DeckList.js
+++ pokemon-deck-generator/src/components/DeckList.js
@@ -5,14 +5,20 @@ import { Link } from 'react-router-dom';
 
 export default function DeckList() {
     
+    const [isLoading, setLoadingState] = useState(false)
     const [decks, setDecks] = useState([])
 
     useEffect(() => {
         Axios.get('http://127.0.0.1:3000/v1/decks/')
+        setLoadingState(true)
         .then(response => {
             setDecks(response.data.payload.decks.reverse())
+            setLoadingState(false)
         })
     }, [])    
+    const shouldDisplayDecks = !isLoading && !!decks.length;
+    const shouldDisplayLoader = isLoading;
+    const displayNoDecksFound = !isLoading && !decks.length;
 
     return (
       <div>
@@ -22,6 +28,24 @@ export default function DeckList() {
                 <Link to={`/decks/${deck.id}`}>Deck #{deck.id}</Link>
             </li>)}
         </ul>
+        {
+          shouldDisplayLoader && 
+          <div>Loading Decks...</div>
+        }
+
+        {shouldDisplayDecks &&
+          <ul>
+              {decks.map((deck) => <li key={deck.id}>
+                  <Link to={`/decks/${deck.id}`}>Deck #{deck.id} - {deck.pokemon_type}</Link>
+              </li>)}
+          </ul>
+        }
+
+        {displayNoDecksFound &&
+          <div>
+            No Decks Found.
+          </div>
+        }
       </div>
     );
   }
