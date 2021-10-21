8dcf76aa053a54224046b79a1af91875c27faede
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2021-10-21 14:05:52 -0400

    add Generator functionality

--- pokemon-deck-generator/src/components/Generator.js
+++ pokemon-deck-generator/src/components/Generator.js
@@ -1,8 +1,69 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import pokemonTypes from "../constants/pokemonTypes";
+
 export default function Generator() {
+    const [isLoading, setLoadingState] = useState(false)
+    const [deck, setDeck] = useState(null)
+    const [pokemonType, setPokemonType] = useState(pokemonTypes[0])
+    const [numberOfPokemon, setNumberOfPokemon] = useState(12)
+
+    const handleGeneratedDeck = deck => {
+        setDeck(deck)
+    }
+
+    const handleFormSubmit = event => {
+        event.preventDefault();
+        setLoadingState(true)
+        // Generate Deck
+        const postData = {
+            pokemon_type: pokemonType,
+            number_of_pokemon: numberOfPokemon
+        }
+        axios.post('http://127.0.0.1:3000/v1/decks', postData)
+            .then(response => {
+                setLoadingState(false)
+                handleGeneratedDeck(response.data.deck)
+            })
+    }
     
+    const shouldDisplayLoader = isLoading;
+    const shouldDisplayDeck = !isLoading && deck;
+    const shouldDisplayForm = !isLoading && !deck;
     return (
       <div>
         <h2>Generator</h2>
+        {shouldDisplayForm &&
+            <form onSubmit={handleFormSubmit}>
+                <fieldset>
+                    <label htmlFor="pokemon_type">Select Pokemon Type</label>
+                    <select id="pokemon_type" value={pokemonType} onChange={e => setPokemonType(e.target.value)}>
+                        {pokemonTypes.map(type => <option key={type} value={type}>{type}</option>)}
+                    </select>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="number_of_cards">Select number of Pokemon Cards in deck</label>
+                    <input id="number_of_cards" type="range" min="12" max="16" value={numberOfPokemon} onChange={e => setNumberOfPokemon(e.target.value)}>
+
+                    </input>
+                    <strong>{numberOfPokemon}</strong>
+                </fieldset>
+                <button type="submit">Generate Deck</button>
+            </form>
+        }
+        {shouldDisplayDeck &&
+            <div>
+                <h3>Deck Generated! Deck ID is #{deck.id}</h3>
+                <div>
+                    <Link to={`/decks/${deck.id}`}>Click Here to View</Link>
+                </div>
+            </div>
+        }
+        {shouldDisplayLoader &&
+            <div>Loading...</div>
+        }
+
       </div>
     );
   }
