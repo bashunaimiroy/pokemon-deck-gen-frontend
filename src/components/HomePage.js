2932c1f0ed26f7d7fe268abfa2b315d4dabf6e8e
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2021-10-21 19:47:47 -0400

    add text to Homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,11 @@ export default function HomePage() {
     return (
       <div>
         <h2>Home</h2>
+        <div>
+          Welcome to the Pokemon Deck Generator!
+          Click "Decks" to view all existing decks,
+          or click "Generate" to generate your own.
+        </div>
       </div>
     );
   }
