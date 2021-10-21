9a4fe854f72a550f0a5554583c87385cd10aa0bd
Bashu Naimi-Roy <bashunaimiroy@gmail.com>
2021-10-20 22:47:22 -0400

    add initial routes

--- pokemon-deck-generator/src/App.js
+++ pokemon-deck-generator/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DeckList from "./components/DeckList";
+import Generator from "./components/Generator";
+import HomePage from "./components/HomePage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import DeckPage from "./components/DeckPage";
+  
+  export default function PokemonGeneratorApp() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/deckList">DeckList</Link>
+            </li>
+            <li>
+              <Link to="/generator">Generator</Link>
+            </li>
+          </ul>
+  
+          <hr />
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/deckList">
+              <DeckList />
+            </Route>
+            <Route path="/generator">
+              <Generator />
+            </Route>
+            <Route path="/decks/:id">
+              <DeckPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
+ 
+  
